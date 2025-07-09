f3cdd337e6eaee769a97ff98ed24daaddba31541
olababiuch <ola.babiuch7@gmail.com>
2025-07-09 13:45:28 -0400

    part A

---
+++ src/SafeInput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class SafeInput {
+    public static String getNonZeroLenString(Scanner pipe, String prompt) {
+        String retString = "";
+        do {
+            System.out.print("\n" + prompt + ": ");
+            retString = pipe.nextLine();
+        } while (retString.length() == 0);
+        return retString;
+    }
+}
