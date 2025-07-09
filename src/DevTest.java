e6099be2f8d8b65aec346abbdaaf0aa398e57733
olababiuch <ola.babiuch7@gmail.com>
2025-07-09 16:24:06 -0400

    part g

--- src/DevTest.java
+++ src/DevTest.java
@@ -3,8 +3,37 @@ import java.util.Scanner;
 public class DevTest {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String name = SafeInput.getNonZeroLenString(in, "Enter your name");
-        System.out.println("You entered: " + name);
-    }
+        //String name = SafeInput.getNonZeroLenString(in, "Enter your name");
+        //System.out.println("You entered: " + name);
 
+        //int age = SafeInput.getInt(in, "Enter your age");
+        //System.out.println("You entered: " + age);
+
+        //double value = SafeInput.getDouble(in, "Enter any double value");
+        //System.out.println("You entered: " + value);
+
+        //int age = SafeInput.getRangedInt(in, "Enter your age", 18, 100);
+        //System.out.println("You entered: " + age);
+
+        //double grade = SafeInput.getRangedDouble(in, "Enter your grade", 0, 100);
+        //System.out.println("You entered: " + grade);
+
+        //boolean answer = SafeInput.getYNConfirm(in, "Do you want to continue?");
+        //System.out.println("You answered: " + answer);
+
+        String ssn = SafeInput.getRegExString(in, "Enter your SSN (###-##-####)" , "^\\d{3}-\\d{2}-\\d{4}$");
+        System.out.println("You entered: " + ssn);
+
+        String mNumber = SafeInput.getRegExString(in, "Enter your UC Student M number (M#####)", "^(M|m)\\d{5}$");
+        System.out.println("You entered: " + mNumber);
+
+        String menu = SafeInput.getRegExString(in, "Enter menu choice [O,S,V,Q]", "^[OoSsVvQq]$");
+        System.out.println("You entered: " + menu);
+
+
+
+
+
+
+    }
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -8,4 +8,114 @@ public class SafeInput {
         } while (retString.length() == 0);
         return retString;
     }
+
+    public static int getInt(Scanner pipe, String prompt) {
+        int retValue = 0;
+        boolean done = false;
+        do {
+            System.out.print("\n" + prompt + ": ");
+            if (pipe.hasNextInt()) {
+                retValue = pipe.nextInt();
+                pipe.nextLine();
+                done = true;
+            } else {
+                String trash = pipe.nextLine();
+                System.out.println("You must enter an integer, not " + trash);
+            }
+        } while (!done);
+        return retValue;
+    }
+
+    public static double getDouble(Scanner pipe, String prompt) {
+        double retValue = 0;
+        boolean done = false;
+        do {
+            System.out.print("\n" + prompt + ": ");
+            if (pipe.hasNextDouble()) {
+                retValue = pipe.nextDouble();
+                pipe.nextLine();
+                done = true;
+            } else {
+                String trash = pipe.nextLine();
+                System.out.println("You must enter a double, not " + trash);
+            }
+        } while (!done);
+        return retValue;
+
+    }
+
+    public static int getRangedInt(Scanner pipe, String prompt, int low, int high) {
+        int retValue = 0;
+        boolean done = false;
+        do {
+            System.out.print("\n" + prompt + " [" + low + " - " + high + "]: ");
+            if (pipe.hasNextInt()) {
+                retValue = pipe.nextInt();
+                pipe.nextLine();
+                if (retValue >= low && retValue <= high) {
+                    done = true;
+                } else {
+                    System.out.println("Input must be between " + low + " and " + high);
+                }
+            } else {
+                String trash = pipe.nextLine();
+                System.out.println("You must enter an integer, not " + trash);
+            }
+        } while (!done);
+        return retValue;
+    }
+    public static double getRangedDouble(Scanner pipe, String prompt, double low, double high) {
+        double retValue = 0;
+        boolean done = false;
+        do {
+            System.out.print("\n" + prompt + " [" + low + " - " + high + "]: ");
+            if (pipe.hasNextDouble()) {
+                retValue = pipe.nextDouble();
+                pipe.nextLine();
+                if (retValue >= low && retValue <= high) {
+                    done = true;
+                } else {
+                    System.out.println("Input must be between " + low + " and " + high);
+                }
+            } else {
+                String trash = pipe.nextLine();
+                System.out.println("You must enter a double value, not " + trash);
+            }
+        } while (!done);
+        return retValue;
+    }
+    public static boolean getYNConfirm(Scanner pipe, String prompt) {
+        String response = "";
+        boolean valid = false;
+        do {
+            System.out.print("\n" + prompt + " [Y/N]: ");
+            response = pipe.nextLine();
+            if (response.equalsIgnoreCase("Y")) {
+                valid = true;
+                return true;
+            } else if (response.equalsIgnoreCase("N")) {
+                valid = false;
+                return false;
+            } else {
+                System.out.println("You must enter Y or N");
+            }
+        } while (!valid);
+        return false;
+    }
+    public static String getRegExString(Scanner pipe, String prompt, String regEx) {
+        String response = "";
+        boolean valid = false;
+        do {
+            System.out.print("\n" + prompt + ": ");
+            response = pipe.nextLine();
+            if (response.matches(regEx)) {
+                valid = true;
+            } else {
+                System.out.println("Input does not match the required pattern");
+            }
+        } while (!valid);
+        return response;
+    }
+
+
 }
